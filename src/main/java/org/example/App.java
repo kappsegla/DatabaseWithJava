f5f110e2abb1876811aa9f8b7aeb973937b50d74
Martin Blomberg <martin.blomberg@outlook.com>
2022-02-23 14:49:07 +0100

    Add first statement

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.sql.*;
+
 public class App {
+
+    private static final String sqlQuery = "Select * from pet";
+
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newton?user=user&password=password"))
+        {
+            //Do something with connection..
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            ResultSet resultSet = statement.executeQuery();
+
+
+
+            resultSet.close();
+            statement.close();
+        }catch(SQLException sqlException){
+            System.out.println("Error with database");
+        }
     }
 }
