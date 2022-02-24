f098547161d2f0fe6a84cca52478c07f4b78a440
Martin Blomberg <martin.blomberg@outlook.com>
2022-02-24 13:48:23 +0100

    Add hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+
+
+
+
+
     }
 }
