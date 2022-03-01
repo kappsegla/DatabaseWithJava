0801afcf0cef2ac6e86ddd3e6f6d4fc32cd0180c
Martin Blomberg <martin.blomberg@outlook.com>
2022-03-01 11:14:37 +0100

    JavaFX OnAction

--- src/main/java/org/example/HelloFX.java
+++ src/main/java/org/example/HelloFX.java
@@ -3,6 +3,7 @@ package org.example;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -11,13 +12,21 @@ public class HelloFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        TextField textField = new TextField();
+
         Button button = new Button();
         button.setText("Click on me!");
+        button.setOnAction(actionEvent -> {
+            button.setText(textField.getText());
+        });
+
 
         VBox vBox = new VBox();
+
+        vBox.getChildren().add(textField);
         vBox.getChildren().add(button);
 
-        Scene scene = new Scene(vBox);
+        Scene scene = new Scene(vBox,400,400);
 
         stage.setScene(scene);
         stage.show();
