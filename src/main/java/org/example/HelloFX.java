3e47bf5b79f4776394b300345d2d145cfc8b2ff8
Martin Blomberg <martin.blomberg@outlook.com>
2022-03-01 11:48:28 +0100

    JavaFX label textfield

--- src/main/java/org/example/HelloFX.java
+++ src/main/java/org/example/HelloFX.java
@@ -2,8 +2,8 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,21 +12,23 @@ public class HelloFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+        Label label = new Label("_Name:");
         TextField textField = new TextField();
-
+        label.setMnemonicParsing(true);
+        label.setLabelFor(textField);
         Button button = new Button();
         button.setText("Click on me!");
         button.setOnAction(actionEvent -> {
             button.setText(textField.getText());
         });
 
+        HBox hBox = new HBox();
 
-        VBox vBox = new VBox();
+        hBox.getChildren().add(label);
+        hBox.getChildren().add(textField);
+        hBox.getChildren().add(button);
 
-        vBox.getChildren().add(textField);
-        vBox.getChildren().add(button);
-
-        Scene scene = new Scene(vBox,400,400);
+        Scene scene = new Scene(hBox, 400, 400);
 
         stage.setScene(scene);
         stage.show();
