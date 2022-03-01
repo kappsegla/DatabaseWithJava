a01c60bed3adebd4f0cea38c8e9319cf77911286
Martin Blomberg <martin.blomberg@outlook.com>
2022-03-01 13:42:25 +0100

    ListView with Strings

--- src/main/java/org/example/HelloFX.java
+++ src/main/java/org/example/HelloFX.java
@@ -1,14 +1,23 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HelloFX extends Application {
+
+    private ObservableList<String> observableList;
+    private ListView<String> listView;
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -17,18 +26,33 @@ public class HelloFX extends Application {
         label.setMnemonicParsing(true);
         label.setLabelFor(textField);
         Button button = new Button();
-        button.setText("Click on me!");
+        button.setText("Add");
         button.setOnAction(actionEvent -> {
-            button.setText(textField.getText());
+            observableList.add(textField.getText());
         });
+        Button deleteButton = new Button();
+        deleteButton.setText("Delete");
+        deleteButton.setOnAction(actionEvent -> {
+            var index = listView.getSelectionModel().getSelectedIndex();
+            if( index >= 0)
+                observableList.remove(index);
+        });
+
+        List<String> stringList = new ArrayList<>(List.of("Hej","Hello","Hola","Guten tag","Ahoj"));
+
+        listView = new ListView<>();
+        observableList = FXCollections.observableList(stringList);
+        listView.setItems(observableList);
 
         HBox hBox = new HBox();
 
         hBox.getChildren().add(label);
         hBox.getChildren().add(textField);
         hBox.getChildren().add(button);
+        hBox.getChildren().add(deleteButton);
+        hBox.getChildren().add(listView);
 
-        Scene scene = new Scene(hBox, 400, 400);
+        Scene scene = new Scene(hBox);
 
         stage.setScene(scene);
         stage.show();
