1acd5554403ea89770c5244bf2c8ac0593f4d720
Martin Blomberg <martin.blomberg@outlook.com>
2022-03-04 11:47:32 +0100

    Add style.css

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,30 +1,48 @@
 package org.example;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class Controller {
     public ColorPicker colorPicker;
     public Canvas canvas;
+    public Button circleButton;
+    public Button rectButton;
+    public TextField textField;
+
     private ShapeType shapeType = ShapeType.CIRCLE;
+    public BooleanProperty enableOnlyOneButton = new SimpleBooleanProperty();
+    public StringProperty textFieldString = new SimpleStringProperty();
+
 
     public void initialize() {
         colorPicker.setValue(Color.BLACK);
+        circleButton.disableProperty().bind(enableOnlyOneButton);
+        rectButton.disableProperty().bind(enableOnlyOneButton.not());
     }
 
     public void circleClicked(ActionEvent actionEvent) {
         shapeType = ShapeType.CIRCLE;
+        enableOnlyOneButton.setValue(true);
     }
 
     public void rectClicked(ActionEvent actionEvent) {
         shapeType = ShapeType.RECT;
+        enableOnlyOneButton.setValue(false);
     }
 
     public void lineClicked(ActionEvent actionEvent) {
         shapeType = ShapeType.LINE;
+        enableOnlyOneButton.setValue(false);
     }
 
     public void canvasClicked(MouseEvent event) {
